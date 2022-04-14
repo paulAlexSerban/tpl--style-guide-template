db933b6ccbf4eb30e795b0bde8ab0ee8878bfbb0
paulAlexSerban <paul.alex.serban@gmail.com>
2022-04-14 04:04:48 +0300

    added express js docker file

--- server/.bash
+++ server/.bash
@@ -8,6 +8,14 @@ function install-static () {
   docker-compose --env-file ./server/.env -f ./server/dist/docker-compose.yml up -d --build
 }
 
+function install-express () {
+  if [ ! -d ./server/dist/ ]; then
+    mkdir -p ./server/dist/
+  fi
+  cp -rfv ./server/source/docker/express/* ./server/dist/ &&
+  docker-compose --env-file ./server/.env -f ./server/dist/docker-compose.yml up -d --build
+}
+
 function stop () {
   docker-compose -f ./server/dist/docker-compose.yml down -v
 }
